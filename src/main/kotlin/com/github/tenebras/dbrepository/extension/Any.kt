921c031aa1c5197bf7e86c72687a09786739cba0
Oleksandr Kostynenko <oleksandr.kostynenko@sixt.com>
2018-07-17 11:39:46 +0300

    allow excluding fields in toJson without extending

--- src/main/kotlin/com/github/tenebras/dbrepository/extension/Any.kt
+++ src/main/kotlin/com/github/tenebras/dbrepository/extension/Any.kt
@@ -3,13 +3,16 @@ package com.github.tenebras.dbrepository.extension
 import com.github.tenebras.dbrepository.gson.gson
 import kotlin.reflect.full.declaredMemberProperties
 
-fun Any?.toJson(): String = gson().toJson(this)
+fun Any?.toJson(exclude: List<String> = emptyList()): String =
+        if (exclude.isEmpty()) {
+            gson().toJson(this)
+        } else {
+            gson().toJson(this.toMap(exclude))
+        }
 
-fun Any?.toJson(extension: Map<String, Any>, exclude: List<String> = emptyList()): String
-        = gson().toJson(this.toMap(exclude) + extension)
+fun Any?.toJson(extension: Map<String, Any>, exclude: List<String> = emptyList()): String = gson().toJson(this.toMap(exclude) + extension)
 
-fun Any?.toJson(extension: Any, exclude: List<String> = emptyList()): String
-        = gson().toJson(this.toMap(exclude) + extension.toMap())
+fun Any?.toJson(extension: Any, exclude: List<String> = emptyList()): String = gson().toJson(this.toMap(exclude) + extension.toMap())
 
 fun Any?.toMap(exclude: List<String> = emptyList()): Map<String, Any?> {
     if (this == null) {
